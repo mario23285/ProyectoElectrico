9f132a7ee4cbeb2b542eccf65d54c2ade3f43d11
Mario Castresana <mc.avendan@gmail.com>
2017-06-07 00:44:07 -0600

    agregar pruebas

--- README.md
+++ README.md
@@ -7,4 +7,22 @@ A41267
 I-2017
 
 
+#Introducción
 
+#Programa y modo de empleo
+El programa implementa un algoritmo de corrección de glitches de MoCap, basado
+en estudios de goniometría y fisiología articular.  Mediante dichos estudios, el script
+es capaz de entender los movimientos de un ser humano y determinar si un archivo BVH
+contiene errores de captura, los cuales se manifiestan como movimientos erráticos, quebaraduras
+de articulaciones o movimientos imposibles de ejecutar para un ser humano.
+
+El script se localiza en la carpeta /src y no requiere de instalación o dependencias de software.
+Simplemente copie el archivo bvh dañado a la carpeta /src y ejecute la siguiente linea de comando
+en un PowerShell (Windows) [bash en Linux o Mac]
+
+\> python .\BVH_TuneUP.py <BVH_a_corregir> <Nombre del BVH de salida>
+
+Ejemplo:
+    \src> python .\BVH_TuneUp.py .\escena2.2_esqueleto9.bvh .\escena_corregida.bvh
+
+Se generará a la salida un reporte de Glitches (Errores) y el BVH con la escena corregida
---
+++ pruebas/validacion.py
@@ -0,0 +1,88 @@
+"""
+UNIVERSIDAD DE COSTA RICA                       Escuela de Ingeniería Eléctrica
+
+IE0499 | Proyecto Eléctrico
+Mario Alberto Castresana Avendaño
+A41267
+
+Programa: Validacion
+-------------------------------------------------------------------------------
+
+archivo: validacion.py
+descripción:
+Este es un script que compara dos archivos BVH y calcula el error cuadrático medio
+entre cada frame del video representado en sus arreglos de MOTION.  Note que este
+script usa la función zip() la cual viene por default incluida en python 3.
+
+https://github.com/mario23285/ProyectoElectrico.git
+
+"""
+
+"""
+Primero debemos abrir los dos BVH de interés, los cuales se pasan por consola en el 
+orden específico
+
+$ python validacion.py <BVH_corregido_por_BVHTuneUP> <BVH_corregido_manual> <salida_CSV>
+
+Donde la salida_CSV es un archivo de formato Comma Separated Values que se puede analizar
+en Excel o LibreOffice Calc.
+
+"""
+import sys
+import csv
+import re
+
+#Abrir files necesarios
+BVH_corregido = open(sys.argv[1], 'r')
+BVH_manual = open(sys.argv[2], 'r')
+CSV = open('CSVfile.csv', 'w+')
+ecm_csv = csv.writer(CSV, dialect='excel')
+#Contador de frames (cuadros del MoCap)
+frame = 1
+
+
+for c, m in zip(BVH_corregido, BVH_manual):
+
+    if m[0].isdigit() or m[1].isdigit():
+
+        #crear array de 156 posiciones para Error Cuadrático Medio
+        MSE = [frame, 0]
+        #Aquí hay que separar la línea parseada y crear un arreglo de MOTION válido
+        m = re.split('\s+|\n', m)
+        #eliminar el ultimo elemento de m (la linea MOTION manual) '\n'
+        #y convertir a float todo
+        m.pop()
+        MANUAL = [float(nums) for nums in m]
+        print('Procesando BVH MANUAL >> frame: ' + str(frame))
+        #ecm_csv.writerow(MANUAL)
+    else:
+        pass
+
+    if c[0].isdigit() or c[1].isdigit():
+
+        #Aquí hay que separar la línea parseada y crear un arreglo de MOTION válido
+        c = re.split('\s+|\n', c)
+        #eliminar el ultimo elemento de m (la linea MOTION manual) '\n'
+        #y convertir a float todo
+        c.pop()
+        CORREGIDO = [float(nums) for nums in c]
+        print('Procesando BVH CORREGIDO >> frame: ' + str(frame))
+        #ecm_csv.writerow(CORREGIDO)
+
+        #Si existe un arreglo de MOTION en esta línea, sacarle el error cuadráticos medio
+        #en este frame
+        for i in range(len(MANUAL)):
+            MSE[1] += ((CORREGIDO[i]-MANUAL[i])*(CORREGIDO[i]-MANUAL[i]))*(1/len(MANUAL))
+
+        #guardar vector de error cuadrático medio en el CSV
+        ecm_csv.writerow(MSE)
+        #incrementar el contador de frames
+        frame += 1
+
+    else:
+        pass
+
+#liberar memoria
+BVH_corregido.close()
+BVH_manual.close()
+CSV.close()
